387be438b85f27bbde3eb5e8f5ee2d8200df567c
Oscar Michel <oscarmichelh@gmail.com>
2019-10-29 08:25:31 -0600

    Added CORS

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
 angular.module("App", ["main","teams"]);
 angular.module("main", []);
 angular.module("teams", []);
+
+//CORS
+addEventListener('fetch', event => {
+    event.respondWith(handle(event.request))
+})
+
+async function handle(request) {
+    if (request.method === "OPTIONS") {
+        return handleOptions(request)
+    } else if (request.method === "GET" ||
+        request.method === "HEAD" ||
+        request.method === "POST") {
+        // Pass-through to origin.
+        return fetch(request)
+    } else {
+        return new Response(null, {
+            status: 405,
+            statusText: "Method Not Allowed",
+        })
+    }
+}
+
+// We support the GET, POST, HEAD, and OPTIONS methods from any origin,
+// and accept the Content-Type header on requests. These headers must be
+// present on all responses to all CORS requests. In practice, this means
+// all responses to OPTIONS requests.
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, HEAD, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+}
+
+function handleOptions(request) {
+    if (request.headers.get("Origin") !== null &&
+        request.headers.get("Access-Control-Request-Method") !== null &&
+        request.headers.get("Access-Control-Request-Headers") !== null) {
+        // Handle CORS pre-flight request.
+        return new Response(null, {
+            headers: corsHeaders
+        })
+    } else {
+        // Handle standard OPTIONS request.
+        return new Response(null, {
+            headers: {
+                "Allow": "GET, HEAD, POST, OPTIONS",
+            }
+        })
+    }
+}
