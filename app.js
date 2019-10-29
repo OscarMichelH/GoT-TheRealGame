a3832ae8b5619114d49cac294f3273d3e62ec5c8
Oscar Michel <oscarmichelh@gmail.com>
2019-10-29 08:39:55 -0600

    Added font by js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 angular.module("App", ["main","teams"]);
 angular.module("main", []);
 angular.module("teams", []);
+
+var junction_font = new FontFace('GoT', 'url(fonts/got.ttf)');
+junction_font.load().then(function(loaded_face) {
+    document.fonts.add(loaded_face);
+}).catch(function(error) {
+    // error occurred
+});
